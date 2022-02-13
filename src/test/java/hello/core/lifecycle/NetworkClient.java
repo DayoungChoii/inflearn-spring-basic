8d0cef2851322677ee17043c50706fc13e509c2a
DayoungChoii <young12430@gmail.com>
2022-02-13 22:55:43 +0900

    [스프링 핵심 원리 - 기본편] 빈 생면주기 콜백 - 애노테이 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,11 +1,9 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-import javax.naming.ldap.InitialLdapContext;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -30,16 +28,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close" + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { //의존관계 주입이 끝나
-        System.out.println("NetworkClient.afterPropertiesSet");
+    @PostConstruct
+    public void init() throws Exception { //의존관계 주입이 끝나
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 생성 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
